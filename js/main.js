aef07442b3250d7c55a06258605ffecd3bd1dfc4
angvidal <angvidal@publicisgroupe.net>
2019-08-18 12:54:43 -0400

    in process of making it object oriented

--- js/main.js
+++ js/main.js
@@ -1,71 +1,53 @@
-/* you want to start by creating variables for your elements that are in the DOM
+/* you want to start by creating letiables for your elements that are in the DOM
 this is smart practice because its going to save you time from continuing to write
 "document.getElementById" etc. (remember give clear naming conventions) */
 
 //inputs
-var leftInput = document.getElementById("first-Value");
-var rightInput = document.getElementById("second-Value");
+let leftInput = document.getElementById("first-Value");
+let rightInput = document.getElementById("second-Value");
+//
 
 //buttons
-var additionButton = document.getElementById("addition");
-var subtractionButton = document.getElementById("subtraction");
-var multiplicationButton = document.getElementById("multiplication");
-var divisionButton = document.getElementById("division");
+let additionButton = document.getElementById("addition");
+let subtractionButton = document.getElementById("subtraction");
+let multiplicationButton = document.getElementById("multiplication");
+let divisionButton = document.getElementById("division");
 
 //result
-var result = document.getElementById("result");
+let result = document.getElementById("result");
+// let addButton =
+// let secondVal = parseFloat(rightInput.value);
+// let firstVal = parseFloat(leftInput.value);
 
-//event handler
-additionButton.onclick = add;
-subtractionButton.onclick = subtract;
-multiplicationButton.onclick = multiply;
-divisionButton.onclick = divide;
-
-/*next we want to create our functions that correspond to each event handler
-(the things after the onclick events)*/
-
-function add() {
-  var firstNumToAdd = parseFloat(leftInput.value);
-  var secondNumToAdd = parseFloat(rightInput.value);
-  var sum = firstNumToAdd + secondNumToAdd;
-  console.log(sum);
-  result.innerHTML = sum;
-  clearInputs();
-}
-
-function subtract() {
-  var firstNumToAdd = parseFloat(leftInput.value);
-  var secondNumToAdd = parseFloat(rightInput.value);
-  var sum = firstNumToAdd - secondNumToAdd;
-  console.log(sum);
-  result.innerHTML = sum;
-  clearInputs();
-
-}
-
-function multiply() {
-  var firstNumToAdd = parseFloat(leftInput.value);
-  var secondNumToAdd = parseFloat(rightInput.value);
-  var sum = firstNumToAdd * secondNumToAdd;
-  console.log(sum);
-  result.innerHTML = sum;
-  clearInputs();
-
-}
-
-function divide() {
-  var firstNumToAdd = parseFloat(leftInput.value);
-  var secondNumToAdd = parseFloat(rightInput.value);
-  var sum = firstNumToAdd / secondNumToAdd;
-  console.log(sum);
-  result.innerHTML = sum;
-  clearInputs();
-}
-
-/* creating an extra function to call upon events being
-click im going to call this function in each math function above*/
-
-function clearInputs(){
-  leftInput.value = "";
-  rightInput.value = "";
+const calulator = {
+    // firstVal: parseFloat(leftInput.value),
+    // secondVal: parseFloat(rightInput.value),
+    add: document.getElementById("addition").onclick = function () {
+        let firstVal = parseFloat(leftInput.value);
+        let secondVal = parseFloat(rightInput.value);
+        let sum = firstVal + secondVal;
+          console.log(sum);
+          result.innerHTML = sum;
+    },
+    subtract: document.getElementById("subtraction").onclick = function () {
+        let firstNumToSubtract = parseFloat(leftInput.value);
+        let secondNumToSubtract = parseFloat(rightInput.value);
+        let difference = firstNumToSubtract - secondNumToSubtract;
+          console.log(difference);
+          result.innerHTML = difference;
+    },
+    multiply: document.getElementById("multiplication").onclick = function () {
+        let firstNumToMultiply = parseFloat(leftInput.value);
+        let secondNumToMultiply = parseFloat(rightInput.value);
+        let product = firstNumToMultiply * secondNumToMultiply;
+          console.log(product);
+          result.innerHTML = product;
+    },
+    divide: document.getElementById("multiplication").onclick = function () {
+        let firstNumToDivide = parseFloat(leftInput.value);
+        let secondNumToDivide = parseFloat(rightInput.value);
+        let product = firstNumToDivide * secondNumToDivide;
+          console.log(quotient);
+          result.innerHTML = quotient;
+    },
 }
