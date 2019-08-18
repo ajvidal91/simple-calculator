8974260f99f9cedeb0bd4dbc12a229c7221f2bc0
ajvidal91 <angelvidaldev@gmail.com>
2019-08-18 16:28:18 -0400

    condensed code

--- js/main.js
+++ js/main.js
@@ -5,49 +5,48 @@ this is smart practice because its going to save you time from continuing to wri
 //inputs
 let leftInput = document.getElementById("first-Value");
 let rightInput = document.getElementById("second-Value");
-//
+let result = document.getElementById("result");
 
 //buttons
-let additionButton = document.getElementById("addition");
-let subtractionButton = document.getElementById("subtraction");
-let multiplicationButton = document.getElementById("multiplication");
-let divisionButton = document.getElementById("division");
+let add;
+let subtract;
+let multiply;
+let divide;
+let sum;
+let difference;
+let product;
+let quotient;
+let addition = document.getElementById("addition").onclick;
 
-//result
-let result = document.getElementById("result");
-// let addButton =
-// let secondVal = parseFloat(rightInput.value);
-// let firstVal = parseFloat(leftInput.value);
+
+const inputs = {
+  firstVal: function () {
+    return parseFloat(leftInput.value);
+  },
+  secondVal: function () {
+    return parseFloat(rightInput.value)
+  }
+}
 
 const calulator = {
-    // firstVal: parseFloat(leftInput.value),
-    // secondVal: parseFloat(rightInput.value),
-    add: document.getElementById("addition").onclick = function () {
-        let firstVal = parseFloat(leftInput.value);
-        let secondVal = parseFloat(rightInput.value);
-        let sum = firstVal + secondVal;
-          console.log(sum);
-          result.innerHTML = sum;
-    },
-    subtract: document.getElementById("subtraction").onclick = function () {
-        let firstNumToSubtract = parseFloat(leftInput.value);
-        let secondNumToSubtract = parseFloat(rightInput.value);
-        let difference = firstNumToSubtract - secondNumToSubtract;
-          console.log(difference);
-          result.innerHTML = difference;
-    },
-    multiply: document.getElementById("multiplication").onclick = function () {
-        let firstNumToMultiply = parseFloat(leftInput.value);
-        let secondNumToMultiply = parseFloat(rightInput.value);
-        let product = firstNumToMultiply * secondNumToMultiply;
-          console.log(product);
-          result.innerHTML = product;
-    },
-    divide: document.getElementById("multiplication").onclick = function () {
-        let firstNumToDivide = parseFloat(leftInput.value);
-        let secondNumToDivide = parseFloat(rightInput.value);
-        let product = firstNumToDivide * secondNumToDivide;
-          console.log(quotient);
-          result.innerHTML = quotient;
-    },
+  add: function () {
+    addition = inputs.firstVal() + inputs.secondVal();
+    console.log(addition);
+    result.innerHTML = addition;
+  },
+  subtract: document.getElementById("subtraction").onclick = function () {
+    difference = inputs.firstVal() - inputs.secondVal();
+    console.log(difference);
+    result.innerHTML = difference;
+  },
+  multiply: document.getElementById("multiplication").onclick = function () {
+    product = inputs.firstVal() * inputs.secondVal();
+    console.log(product);
+    result.innerHTML = product;
+  },
+  divide: document.getElementById("division").onclick = function () {
+    quotient = inputs.firstVal() / inputs.secondVal();
+    console.log(quotient);
+    result.innerHTML = quotient;
+  },
 }
